fca082416987ff9bc00ca70d60c86a8d7d9ae52e
Vladimir Ignatov <vladimir.ignatov@childrens.harvard.edu>
2018-09-06 14:25:38 -0400

    Clean up #9

--- generator.js
+++ generator.js
@@ -1,36 +1,8 @@
 const router = require("express").Router({ mergeParams: true });
-const ursa   = require('ursa');
 const crypto = require("crypto");
 const jwk    = require("jwk-lite");
 
 module.exports = router;
-
-router.get("/rsa", (req, res) => {
-
-    let enc = req.query.enc;
-    if (["base64", "binary", "hex", "utf8"].indexOf(enc) == -1) {
-        enc = undefined;
-    }
-
-    // create a pair of keys (a private key contains both keys...)
-    const keys = ursa.generatePrivateKey();
-
-    // reconstitute the private and public keys from a base64 encoding
-    const privatePem = keys.toPrivatePem(enc);
-    const publicPem  = keys.toPublicPem(enc);
-
-    // make a private key, to be used for encryption
-    const privateKey = ursa.createPrivateKey(privatePem, '', enc);
-
-    // make a public key, to be used for decryption
-    const publicKey = ursa.createPublicKey(publicPem, enc);
-
-    res.json({
-        privateKey: privatePem,
-        publicKey : publicPem
-    });
-
-});
 
 router.get("/jwks", (req, res) => {
     let alg = String(req.query.alg || "").toUpperCase();
@@ -54,19 +26,4 @@ router.get("/jwks", (req, res) => {
             res.json(out);
         });
     });
-});
-
-router.get("/random", (req, res) => {
-    
-    let enc = req.query.enc;
-    if (["base64", "binary", "hex", "utf8"].indexOf(enc) == -1) {
-        enc = undefined;
-    }
-
-    let len = +req.query.len;
-    if (isNaN(len) || !isFinite(len) || len < 1 || len > 1024) {
-        len = 32;
-    }
-    
-    res.send(crypto.randomBytes(len).toString(enc));
 });
